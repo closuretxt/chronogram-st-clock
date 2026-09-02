ff11290bd4651f5f4dee1e0418bf57550ca97df9
closure <126289616+closuretext@users.noreply.github.com>
2026-09-02 14:02:30 -0300

    - Delay

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ const extensionFolderPath = `scripts/extensions/third-party/chronogram-st-clock`
 
 // Startup
 jQuery(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // So Recast loads first.
+
     const settingsHtml = await $.get(`${extensionFolderPath}/index.html`);
     const tempDiv = $("<div>").html(settingsHtml);
 
